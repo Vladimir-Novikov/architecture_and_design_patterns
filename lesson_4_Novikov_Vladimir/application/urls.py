de6cdc38006e25b250b8554f47685fb86825716b
Vladimir_Novikov <vovasnew@mail.ru>
2021-08-30 20:40:29 +0500

    lesson_4 creational patterns

---
+++ lesson_4_Novikov_Vladimir/application/urls.py
@@ -0,0 +1,38 @@
+from datetime import date
+from application.views import (
+    Index,
+    Contacts,
+    PageNotFound,
+    Write_to_us,
+    CategoryList,
+    CreateCategory,
+    CoursesList,
+    CreateCourse,
+    CopyCourse,
+)
+
+# Front controllers
+
+
+def today(request):
+    request["data"] = date.today()
+
+
+def user_name(request):
+    request["user"] = "User_1"
+
+
+fronts = [today, user_name]
+
+routes = {
+    "/": Index(),
+    "/contacts/": Contacts(),
+    "/write/": Write_to_us(),
+    "/category_list/": CategoryList(),
+    "/create_category/": CreateCategory(),
+    "/courses_list/": CoursesList(),
+    "/create_course/": CreateCourse(),
+    "/copy_course/": CopyCourse(),
+}
+
+page_404 = PageNotFound()
